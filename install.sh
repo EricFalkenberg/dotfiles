51a70e92243c8930c52264562707999dc1e4ad11
EricFalkenberg <exf4789@rit.edu>
2017-11-20 20:08:04 -0800

    add means to install ctags since Tagbar requires it

--- install.sh
+++ install.sh
@@ -3,3 +3,14 @@ cp .zshrc ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 cp -r plugin/ ~/.vim/
+
+if [ uname -eq "Darwin" ]
+then
+    brew install ctags
+fi
+
+# This assumes you're on a debian distro because I'm lazy
+if [ uname -eq "Linux" ]
+then
+    sudo apt-get install ctags
+fi
